53414134bf67c6ca4571e4c5cc2646e7c4375c60
Nicolas QUINQUENEL <nicolas.quinquenel@etu.univ-nantes.fr>
2017-03-10 23:29:51 +0100

    Création de la structure du code, ajout du Makefile

---
+++ src/Interface/fenetre.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "fenetre.h"
+
+void test() {
+  printf("Test de fonctionnement. (Interface/fenetre.c)\n");
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Interface/fenetre.h"
+
+int main() {
+  printf("Test de fonctionnement. (main.c)\n");
+  test();
+
+  return 0;
+}
