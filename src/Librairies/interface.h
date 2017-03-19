d5738389cfbcc46631b6bc6905a14f11dc21d09d
Mathieu SOYER <mathieu.soyer@etu.univ-nantes.fr>
2017-03-19 20:31:08 +0100

    ajout accents a interface.h

--- src/Librairies/interface.h
+++ src/Librairies/interface.h
@@ -1,47 +1,47 @@
 #pragma once
 
-// Structure d�finissant l'identification de la strat�gie
+// Structure définissant l'identification de la stratégie
 typedef struct
 {
-	char name[30];	// Nom de la strat�gie
+	char name[30];	// Nom de la stratégie
 	char members[6][50];	// Noms des personnes participant au projet.
 } SPlayerInfo;
 
-// Structure d�finissant les caract�ristiques d'une cellule
+// Structure définissant les caractéristiques d'une cellule
 typedef struct SCell
 {
 	int id;	// Id de la cellule
-	int owner;	// Id du joueur qui la poss�de
-	int nbDices;	// Nombre de d�s
+	int owner;	// Id du joueur qui la possède
+	int nbDices;	// Nombre de dés
 
 	struct SCell **neighbors;	// Tableau de poiteur vers des cellules voisines
 	int nbNeighbors;	// Nombre de cellules voisines
 } SCell;
 
-// Structure d�finissant la carte globale du jeu
+// Structure définissant la carte globale du jeu
 typedef struct
 {
   SCell *cells;	// Tableau des cellules
   int nbCells;	// Nombre de cellules
-  unsigned int *stack;	// Nombre de d�s en r�serve par joueur -tableau dim nb joueur)
+  unsigned int *stack;	// Nombre de dés en réserve par joueur -tableau dim nb joueur)
 } SMap;
 
-// Structure d�finissant les param�tres d'un coup jou�
+// Structure définissant les paramètres d'un coup joué
 typedef struct
 {
 	unsigned int cellFrom;
 	unsigned int cellTo;
 } STurn;
 
-// Fonction � appeler au d�but de chaque partie
-// La strat�gie doit compl�ter la structure SPlayerInfo
+// Fonction à appeler au début de chaque partie
+// La stratégie doit compléter la structure SPlayerInfo
 void InitGame(unsigned int id, unsigned int nbPlayer, SPlayerInfo *info);
 
-// Fonction � appeler � chaque tour sur la strat�gie et tant que le retour de fonction est vrai et qu'il n'y a pas d'erreur.
+// Fonction à appeler à chaque tour sur la stratégie et tant que le retour de fonction est vrai et qu'il n'y a pas d'erreur.
 //	* Ne pas oublier pour l'arbitre de dupliquer toute la structure map pour chaque appel !
-//	* En cas d'erreur, r�tablir la carte dans l'�tat initial avant le premier tour du joueur.
-// valeur de retour : bool�en : 0 coups termin�s, 1 structure turn compl�t�e avec un nouveau coup � jouer.
+//	* En cas d'erreur, rétablir la carte dans l'état initial avant le premier tour du joueur.
+// valeur de retour : booléen : 0 coups terminés, 1 structure turn complétée avec un nouveau coup à jouer.
 int PlayTurn(const SMap *map, STurn *turn);
 
-// Fonction � appeler � la fin de chaque partie
+// Fonction à appeler à la fin de chaque partie
 void EndGame(unsigned int idWinner);
