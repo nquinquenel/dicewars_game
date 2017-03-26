eabf57b7d8ff81062a2b2368e966b9c48f8580f5
Mathieu SOYER <mathieu.soyer@etu.univ-nantes.fr>
2017-03-26 01:39:50 +0100

    ajout displayDices -> affichage de l'image des dés de la cellule

--- src/Interface/fenetre.c
+++ src/Interface/fenetre.c
@@ -4,6 +4,9 @@
 #include "jouer.h"
 #include "../Librairies/interfacePerso.h"
 
+int IMG_DICES_W = 20;//la dimension de l'image des dés en largeur
+int IMG_DICES_H = 20; //la dimension de l'image des dés en hauteur
+
 /********************************************************************************************
 *
 * FUNCTION NAME: fenetre
@@ -18,11 +21,11 @@ void fenetre() {
   SDL_Window* window = NULL;
   window = SDL_CreateWindow
   (
-      "DiceWars", SDL_WINDOWPOS_UNDEFINED,
-      SDL_WINDOWPOS_UNDEFINED,
-      800,
-      600,
-      SDL_WINDOW_SHOWN
+    "DiceWars", SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    800,
+    600,
+    SDL_WINDOW_SHOWN
   );
 
   //Tableau de comparaison entre id et joueurs
@@ -57,6 +60,12 @@ void fenetre() {
 
   map = generer_map(renderer, 800, 600, 6, 50, tab_comparaison, tab_id);
 
+  // allocation mémoire pour 2 tableaux de pointeurs pour gérer l'affichage des images de dés des cellules
+  // Tableau de pointeurs de SDL_Surface pour les images de dés
+  SDL_Surface** background_surface_tab = malloc(50*sizeof(SDL_Surface *));
+  // Tableau de pointeurs de SDL_Texture pour les images de dés
+  SDL_Texture** background_texture_tab = malloc(50*sizeof(SDL_Texture *));
+
   SDL_Event e;
 
   int res, cellUn, cellDeux, idJoueurActuel;
@@ -66,13 +75,13 @@ void fenetre() {
       idJoueurActuel = getIdJoueurActuel();
       switch (e.type) {
 
-      //On appuie sur la croix rouge de la fenêtre
-      case SDL_QUIT:
+        //On appuie sur la croix rouge de la fenêtre
+        case SDL_QUIT:
         running = 0;
         break;
 
-      //Clique souris
-      case SDL_MOUSEBUTTONDOWN:
+        //Clique souris
+        case SDL_MOUSEBUTTONDOWN:
         //Phase 0 -> Quand on sélectionne son territoire
         if (phase == 0) {
           //On récupère l'ID du territoire sélectionné
@@ -83,7 +92,7 @@ void fenetre() {
             phase = 1;
           }
 
-        //Phase 1 -> Quand on sélectionne le territoire à attaquer
+          //Phase 1 -> Quand on sélectionne le territoire à attaquer
         } else {
           cellDeux = territoireSelec(e.button.x, e.button.y, tab_id);
           STurn *turn = malloc(sizeof(turn));
@@ -115,32 +124,32 @@ void fenetre() {
         }
         break;
 
-      case SDL_KEYDOWN:
+        case SDL_KEYDOWN:
         switch (e.key.keysym.sym) {
           //Touche entrée
           case SDLK_RETURN:
 
-            //On enlève les bordures internes blanches
-            if (phase == 1) {
-              for (i = 1; i < 799; i++) {
-                for (p = 1; p < 599; p++) {
-                  if (tab_borduresBlanches[i][p] == 1) {
-                    SDL_SetRenderDrawColor(renderer, couleurs[idJoueurActuel][0], couleurs[idJoueurActuel][1], couleurs[idJoueurActuel][2], 0);
-                    SDL_RenderDrawPoint(renderer, i, p);
-                    tab_borduresBlanches[i][p] = 0;
-                  }
+          //On enlève les bordures internes blanches
+          if (phase == 1) {
+            for (i = 1; i < 799; i++) {
+              for (p = 1; p < 599; p++) {
+                if (tab_borduresBlanches[i][p] == 1) {
+                  SDL_SetRenderDrawColor(renderer, couleurs[idJoueurActuel][0], couleurs[idJoueurActuel][1], couleurs[idJoueurActuel][2], 0);
+                  SDL_RenderDrawPoint(renderer, i, p);
+                  tab_borduresBlanches[i][p] = 0;
                 }
               }
-              SDL_RenderPresent(renderer);
-              phase = 0;
             }
+            SDL_RenderPresent(renderer);
+            phase = 0;
+          }
 
-            //On passe au joueur suivant
-            idJoueurActuel++;
-            setIdJoueurActuel(idJoueurActuel, 6);
-            printf("Au joueur %d de jouer\n", getIdJoueurActuel());
+          //On passe au joueur suivant
+          idJoueurActuel++;
+          setIdJoueurActuel(idJoueurActuel, 6);
+          printf("Au joueur %d de jouer\n", getIdJoueurActuel());
 
-            break;
+          break;
         }
         break;
       }
@@ -149,4 +158,46 @@ void fenetre() {
 
   SDL_DestroyWindow(window);
   SDL_Quit();
+}
+
+/********************************************************************************************
+*
+* FUNCTION NAME: displayDices
+*
+* DESCRIPTION: affichage de l'image des dés de la cellule
+*
+* ARGUMENT                  TYPE             DESCRIPTION
+* renderer                  SDL_Renderer*    Permet d'intérargir avec la fenêtre SDL2
+* background_surface_tab    SDL_Surface**    Tableau de pointeurs de SDL_Surface pour les images de dés
+* background_texture_tab    SDL_Texture**    Tableau de pointeurs de SDL_Texture pour les images de dés
+* pixel_x                   int              coordonnée x du pixel central de la cellule
+* pixel_y                   int              coordonnée y du pixel central de la cellule
+* idCEll                    int              l'id de la cellule
+* nbDices                   int              le nombre de dés de la cellule
+*
+*********************************************************************************************/
+void displayDices(SDL_Renderer* renderer, SDL_Surface** background_surface_tab, SDL_Texture** background_texture_tab, int pixel_x, int pixel_y, int idCell, int nbDices)
+{
+  printf("%s\n", "Didier");
+
+  // les objets SDL pour l'image
+  SDL_Surface* background_surface = NULL;
+  SDL_Texture* background_texture = NULL;
+
+  // positionnement et taille de l'image
+  SDL_Rect img_pos;
+  img_pos.x = pixel_x;
+  img_pos.y = pixel_y;
+  img_pos.w = IMG_DICES_W;
+  img_pos.h = IMG_DICES_H;
+
+  background_surface = SDL_LoadBMP("./Interface/images/test3.bmp");
+  background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
+
+  background_surface_tab[idCell] = background_surface;
+  background_texture_tab[idCell] = background_texture;
+
+  SDL_RenderCopy(renderer, background_texture, NULL, &img_pos);
+
+  SDL_RenderPresent(renderer);
 }
