d2c732deb8d3638ab772d5847c8d54e9df15bd09
Mathieu SOYER <mathieu.soyer@etu.univ-nantes.fr>
2017-03-26 01:40:58 +0100

    ajout des documentations de fonctions dans les .h

--- src/Interface/fenetre.h
+++ src/Interface/fenetre.h
@@ -1,3 +1,28 @@
 #include "generation.h"
 
+/********************************************************************************************
+*
+* FUNCTION NAME: fenetre
+*
+* DESCRIPTION: Génère la fenêtre de jeu SDL2 et départ du programme (boucle du jeu)
+*
+*********************************************************************************************/
 void fenetre();
+
+/********************************************************************************************
+*
+* FUNCTION NAME: displayDices
+*
+* DESCRIPTION: affichage de l'image des dés de la cellule
+*
+* ARGUMENT                  TYPE             DESCRIPTION
+* renderer                  SDL_Renderer*    Permet d'intérargir avec la fenêtre SDL2
+* background_surface_tab    SDL_Surface**    Tableau de pointeurs de SDL_Surface pour les images de dés
+* background_texture_tab    SDL_Texture**    Tableau de pointeurs de SDL_Texture pour les images de dés
+* pixel_x                   int              coordonnée x du pixel central de la cellule
+* pixel_y                   int              coordonnée y du pixel central de la cellule
+* idCEll                    int              l'id de la cellule
+* nbDices                   int              le nombre de dés de la cellule
+*
+*********************************************************************************************/
+void displayDices(SDL_Renderer* renderer, SDL_Surface** background_surface_tab, SDL_Texture** background_texture_tab, int pixel_x, int pixel_y, int idCell, int nbDices);
--- src/Interface/generation.h
+++ src/Interface/generation.h
@@ -1,9 +1,82 @@
 #include <SDL2/SDL.h>
 #include "../Librairies/interface.h"
 
+/********************************************************************************************
+*
+* FUNCTION NAME: generer_map
+*
+* DESCRIPTION: Génère la carte du jeu
+*
+* ARGUMENT      TYPE             DESCRIPTION
+* renderer      SDL_Renderer*    Permet d'intérargir avec la fenêtre SDL2
+* h             int              Hauteur de la fenêtre
+* w             int              Largeur de la fenêtre
+* nbJoueurs     int              Nombre de joueurs
+* nbTerritoires int              Nombre de cellules
+*
+*********************************************************************************************/
 SMap* generer_map(SDL_Renderer* renderer, int h, int w, int nbJoueurs, int nbTerritoires, int *tab_comparaison, int** tab_id);
+
+/********************************************************************************************
+*
+* FUNCTION NAME: point_aleatoire
+*
+* DESCRIPTION: Génère un point aléatoire qui définit, à peu prêt, l'emplacement d'une cellule
+*
+* ARGUMENT      TYPE             DESCRIPTION
+* h             int              Hauteur de la fenêtre de jeu
+* w             int              Largeur de la fenêtre de jeu
+* tab           int**            Tableau qui contient les points à générer
+* row           int              Index du point généré
+*
+*********************************************************************************************/
 void point_aleatoire(int h, int w, int **tab, int row);
+
+/********************************************************************************************
+*
+* FUNCTION NAME: couleur_aleatoire
+*
+* DESCRIPTION: Attribut une couleur aléatoire (parmi les 8 par défaut) à une cellule
+*
+* ARGUMENT         TYPE             DESCRIPTION
+* tab              int**            Tableau qui contient les 8 couleurs
+* row              int              Permet de savoir quelle couleur sera choisit
+* couleur_actuelle int*             Permet de connaître l'index de la couleur actuelle, elle augmente et se remet à 0 une fois les 8 couleurs atteintes
+* couleur          int[8][3]        Tableau qui contient les couleurs
+* nbJoueurs        int              Nombre de joueurs
+*
+*********************************************************************************************/
 void couleur_aleatoire(int **tab, int row, int *couleur_actuelle, int couleur[8][3], int nbJoueurs);
+
+/********************************************************************************************
+*
+* FUNCTION NAME: generer_cellules
+*
+* DESCRIPTION: Génère un tableau contenant les structures SCell de la carte
+*
+* ARGUMENT        TYPE             DESCRIPTION
+* tab_adj         int**            Matrice d'adjacence de la carte
+* nbTerritoires   int              Nombre de cellules
+* tab_comparaison int*             Tableau qui permet de savoir quelle ID corresond à quel joueur
+*
+* RETURNS: Retourne un tableau de SCell
+*
+*********************************************************************************************/
 SCell* generer_cellules(int **tab_adj, int nbTerritoires, int *tab_comparaison);
+
+/********************************************************************************************
+*
+* FUNCTION NAME: generer_territoire
+*
+* DESCRIPTION: Génère la structure SMap du jeu
+*
+* ARGUMENT      TYPE             DESCRIPTION
+* cellules      SCell*           Tableau qui contient toutes les cellules de la carte
+* nbTerritoires int              Nombre de cellules
+*
+* RETURNS: Retourne la SMap générée
+*
+*********************************************************************************************/
 SMap* generer_territoire(SCell* cellules, int nbTerritoires);
+
 void lib_memoire();
