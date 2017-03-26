e5de662500e85ee8422a5bb6af34fbbc4b67266f
Arthur Ouhtier <arthur.outhier@etu.univ-nantes.fr>
2017-03-26 15:44:20 +0200

    Ajout de la déclaration de la méthode dans Interfaceperso.h

--- src/Librairies/interfacePerso.h
+++ src/Librairies/interfacePerso.h
@@ -211,3 +211,18 @@ int GetNbPlayerCells(const SMap *map, int idPlayer);
 *
 *********************************************************************************************/
 int AreNeighbors(SCell *cell1, SCell *cell2);
+/********************************************************************************************
+*
+* FUNCTION NAME: MiniCoupSCell
+*
+* DESCRIPTION: fonction permettant de chercher un voisin qui a une diffférence de 1 avec notre cellule
+*
+* ARGUMENT    TYPE             DESCRIPTION
+* voisins     **SCell          Le pointeur du tableau de pointeurs
+* nbVoisins   int              Le nombre de voisins de notre cellule
+* nbDes       int              Le nombre de dés de notre cellule
+*
+* RETURNS: Un tableau de 2 entiers contenant le rang et le nombre de dés
+*
+*********************************************************************************************/
+int* MiniCoupSCell(SCell **voisins, int nbVoisins, int nbDes);
